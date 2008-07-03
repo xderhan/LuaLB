527a24296fa3d78e94e9d74313fde4ece1068e6c
ngamsaad.waipot <ngamsaad.waipot@9cca8e48-0051-0410-93b1-699f9ea6b622>
2008-07-03 21:57:27 +0000

    apply typemap in Graphics module which leads to easy passing color array from Lua to C

--- trunk/lualb/src/graphics/lb-colormap.h
+++ trunk/lualb/src/graphics/lb-colormap.h
@@ -9,14 +9,11 @@ typedef struct _LBColormap LBColormap;
 
 LBColormap* LBColormap_new(void);
 void LBColormap_destroy(LBColormap*);
-
 int LBColormap_num_colors(const LBColormap*);
-
-void _LBColormap_get_color(const LBColormap*, int, double c[3]);
-void _LBColormap_set_color(LBColormap*, int, const double c[3]);
+void LBColormap_get_color(const LBColormap*, int, double c[3]);
+void LBColormap_set_color(LBColormap*, int, const double c[3]);
 void LBColormap_append_color(LBColormap*, const double c[3]);
-
-void _LBColormap_map_value(const LBColormap*, double, double c[3]);
+void LBColormap_map_value(const LBColormap*, double, double c[3]);
 
 LB_END_DECLS
 

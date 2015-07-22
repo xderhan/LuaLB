c1844fd51ec76e7a5113f46b976da22c25fbd66d
Sederhana Gulo <admin@x-coder.mobi>
2015-07-23 01:09:42 +0700

    Fix the version code and add the new author. Try to build but failed

--- config.h
+++ config.h
@@ -44,25 +44,25 @@
 #define PACKAGE "lualb"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "ngamsaad.waipot@gmail.com"
+#define PACKAGE_BUGREPORT "admin@x-coder.mobi"
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "lualb"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "lualb 0.1-alpha1"
+#define PACKAGE_STRING "lualb 0.1.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "lualb"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.1-alpha1"
+#define PACKAGE_VERSION "0.1.1"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.1-alpha1"
+#define VERSION "0.1.1"
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */
